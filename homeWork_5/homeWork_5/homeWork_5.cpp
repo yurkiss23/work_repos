9208071eb28a75487203cb9f262cdcf565b03a2f
Администратор <@TEST53>
2017-12-26 14:10:01 +0200

    add hw_5 ex.3

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -30,6 +30,23 @@ int main()
 		cout << endl;
 	}*/
 
+	//ex.3
+	/*int up_let = 0, low_let = 0, dig = 0;
+	char symb;
+	for (int i = 0; i < 7; ++i) {
+		cin >> symb;
+		if (symb >= 65 && symb <= 90) {
+			up_let++;
+		}
+		else if (symb >= 97 && symb <= 122) {
+			low_let++;
+		}
+		else if (symb >= 48 && symb <= 57) {
+			dig++;
+		}
+	}
+	cout << "\nuppercase: " << up_let << "\nlowercase: " << low_let << "\ndigits: " << dig << endl;*/
+
 
 	return 0;
 }
