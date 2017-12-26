39f0f4281069d754d8a198382acba9dcd60b91c7
Администратор <@TEST53>
2017-12-26 12:39:58 +0200

    add ex.2

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 int main()
 {
-	
+	//ex.2
+	int start = 0, end = 127;
+	for (int i = start; i <= end; ) {
+		for (int j = 0; j < 16; ++j) {
+			char symb = i;
+			cout << symb;
+			i++;
+		}
+		cout << endl;
+	}
 	return 0;
 }
