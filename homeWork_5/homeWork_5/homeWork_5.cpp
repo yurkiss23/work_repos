7fed18261eba60c32ae70e19deadc07150ec7936
Администратор <@TEST53>
2017-12-26 12:06:31 +0200

    add hw_5 ex.1

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main()
 {
-	
+	//ex.1
+	float t, t_max=0, t_min=100;
+	int i, d_max=0, d_min;
+	for (i = 1; i <= 7; ++i) {
+		cin >> t;
+		if (t > t_max) {
+			t_max = t;
+			d_max = i;
+		}
+		if (t < t_min) {
+			t_min = t;
+			d_min = i;
+		}
+	}
+	cout << "max temperature: " << d_max << "day " << t_max << "degrees" << endl;
+	cout << "min temperature: " << d_min << "day " << t_min << "degrees" << endl;
 	return 0;
 }
