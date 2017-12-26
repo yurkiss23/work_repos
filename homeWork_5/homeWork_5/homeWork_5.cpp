a02b88395c3e6dac3a7cab1a07a391eebe8fcd5f
Администратор <@TEST53>
2017-12-26 14:32:07 +0200

    add hw_5 ex.4

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 int main()
 {
-	
+	//ex.4
+	int dig, num, total_1 = 0, total_7 = 0;
+	cin >> num;
+	for (int i = 0; i < num; ++i) {
+		cin >> dig;
+		if (dig % 10 == 1) {
+			total_1++;
+		}
+		if (dig % 10 == 7) {
+			total_7++;
+		}
+	}
+	cout << "\nlast digit 1: " << total_1 << "\nlast digit 7: " << total_7 << endl;
 	return 0;
 }
