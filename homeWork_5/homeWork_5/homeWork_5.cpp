1d18da80987979d3b55ef44626968ba77b704734
Администратор <@TEST53>
2017-12-26 15:19:17 +0200

    add hw_ ex.5

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main()
 {
-	
+	//ex.5
+	int N;
+	float sum=0, add = 1;
+	cin >> N;
+	for (int i = 1; i <= N; ) {
+		sum += add;
+		cout << "\nsum: " << sum << " add: " << add;
+		i++;
+		add = sqrt(i);
+	}
+	cout << "\ntotal sum: " << sum << endl;
 	return 0;
 }
