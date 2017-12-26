7b803cc995c46d4e3e5e4590358977f41f306e49
Администратор <@TEST53>
2017-12-26 14:35:50 +0200

    add hw_5 ex.4

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -47,6 +47,18 @@ int main()
 	}
 	cout << "\nuppercase: " << up_let << "\nlowercase: " << low_let << "\ndigits: " << dig << endl;*/
 
-
+	//ex.4
+	/*int dig, num, total_1 = 0, total_7 = 0;
+	cin >> num;
+	for (int i = 0; i < num; ++i) {
+		cin >> dig;
+		if (dig % 10 == 1) {
+			total_1++;
+		}
+		if (dig % 10 == 7) {
+			total_7++;
+		}
+	}
+	cout << "\nlast digit 1: " << total_1 << "\nlast digit 7: " << total_7 << endl;*/
 	return 0;
 }
