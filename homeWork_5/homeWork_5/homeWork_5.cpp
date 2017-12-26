7000097239abca8110387235e07a180c3afaab92
Администратор <@TEST53>
2017-12-26 18:02:34 +0200

    add hw_5 ex.6

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 int main()
 {
-	
+	//ex.6
+	int max = 10, num_a = 0;
+	char symb;
+	for (int i = 0; i < max; ++i) {
+		cin >> symb;
+		if (symb == 'a') {
+			num_a++;
+		}
+		//v.1
+		if (symb == '.') {
+			cout << "input stop" << endl;
+			break;
+		}
+		//v.2
+		/*if (symb == '.') {
+			cout << "input stop" << endl;
+			i = max;
+		}*/
+	}
+	cout <<"\nnumber of \'a\': "<< num_a;
 	return 0;
 }
