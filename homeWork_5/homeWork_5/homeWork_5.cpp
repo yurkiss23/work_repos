2fe9a25581932338aff2847e971b0e998665f806
Администратор <@TEST53>
2017-12-27 17:10:09 +0200

    add hw_5 ex.7

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
-	
+	//ex.7
+	unsigned int dig;
+	int sum = 0, num = 0, a;
+	cin >> dig;
+	for (;dig%10;) {
+		a = dig % 10;
+		dig /= 10;
+		sum += a;
+		num++;
+		cout << a << endl;
+	}
+	cout << "sum: " << sum << "\nnumber: " << num;
 	return 0;
 }
