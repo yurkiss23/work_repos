cc94a324988b3554d5abe0609ddb6c0f81fa2e2f
Администратор <@TEST53>
2017-12-28 17:17:08 +0200

    add hw_5 ex.12

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
-	
+	//ex.12
+	int N, h = 1, i;
+	cin >> N;
+	for (i = 1; i < N; ++i, N-=h) {
+		cout << "den: " << i << " vismiknuto: " << h << " kilkist volosin: " << (N - h) << endl;
+		h *= 2;
+	}
+	cout << "volossja zakinchitsja za " << i << " dniv" << endl;
 	return 0;
 }
