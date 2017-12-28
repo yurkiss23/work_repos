b021af8e03090cb1286ee112cf5ed9a03b4256a2
Администратор <@TEST53>
2017-12-28 15:52:39 +0200

    add hw_5 ex.9

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 int main()
 {
-	
+	//ex.9
+	unsigned int dig;
+	int sum = 0;
+	cout << "input digit: ";
+	cin >> dig;
+	if (dig != 1) {
+		for (int i = 1; i <= dig; ++i) {
+			if (dig%i == 0) {
+				sum++;
+			}
+		}
+		if (sum == 2) {
+			cout << "chislo proste" << endl;
+		}
+		else {
+			cout << "chislo scladene" << endl;
+		}
+	}
+	else {
+		cout << "vvedeno 1" << endl;
+	}
 	return 0;
 }
