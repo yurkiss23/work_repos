a6b1d84f1f31c674fcda7545e4a592b195eecb55
Администратор <@TEST53>
2017-12-28 16:59:29 +0200

    add hw_5 ex.11

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 int main()
 {
-	
+	//ex.11
+	int N, s_vas, s_gor;
+	cout << "kilkist xodiv: ";
+	cin >> N;
+	for (int i = 1; i <= N; ++i) {
+		if (i == 1) {
+			s_vas = 3;
+			s_gor = 5;
+			cout << "xid: " << i << " vasilina: " << s_vas << " gorinich: " << s_gor << endl;
+		}
+		else {
+			s_vas += 6;
+			s_gor *= 2;
+			cout << "xid: " << i << " vasilina: " << s_vas << " gorinich: " << s_gor << endl;
+		}
+	}
+	cout << "gorinich prokovtnuv " << (s_vas + s_gor) << " shashok" << endl;
 	return 0;
 }
