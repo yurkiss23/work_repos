c56db9799d8af7bc0cb7b402f8192b62d9c05707
Администратор <@TEST53>
2017-12-28 14:58:03 +0200

    add hw_5 ex.8

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 int main()
 {
-	
+	//ex.8
+	cout << "\t..::Cell-phone::.." << endl;
+	const int PIN = 1111;
+	int pin, i = 1, action;
+	enum actions{cash=1,call,send,lock};
+	cout << "enter pin: ";
+	cin >> pin;
+	//while (i < 3) {
+		if (pin == PIN) {
+			do {
+				cout << "select action: 1-add maney, 2-call, 3-send sms, 4-lock your phone" << endl;
+				cin >> action;
+				switch (action) {
+				case actions::cash:cout << "add money!" << endl; break;
+				case actions::call:cout << "enter phone number" << endl; break;
+				case actions::send:cout << "enter text" << endl; break;
+				//case actions::lock:cout << "locked!" << endl; break; continue;
+				}
+			} while (action != 4);
+		}
+		else {
+			cout << "invalid pin! try again!" << endl;
+			cin >> pin;
+			i++;
+		}
+	//}
+	cout << "bay!" << endl;
 	return 0;
 }
