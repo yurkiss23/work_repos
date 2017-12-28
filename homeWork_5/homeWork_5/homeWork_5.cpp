b68026ad37c5277088a3cd528efcc5043143d9f6
Администратор <@TEST53>
2017-12-28 17:52:47 +0200

    add hw_5 ex.13

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -176,5 +176,18 @@ int main()
 		h *= 2;
 	}
 	cout << "volossja zakinchitsja za " << i << " dniv" << endl;*/
+
+	//ex.13
+	/*double N, rest;
+	int M, i, j;
+	cout << "input leght and stopday: ";
+	cin >> N >> M;
+	rest = N;
+	for (i = 1, j = 2; i != M; ++i, ++j) {
+		N /= j;
+		rest -= N;
+		cout << "den: " << i << " z'jv: " << N << " zalishilos: " << rest << endl;
+	}
+	cout << "na den: " << i << " zalishilos z'jsti: " << rest << "km" << endl;*/
 	return 0;
 }
