591ee2c5dd79f314496b4a0a8ca4920452c7e1dd
Администратор <@TEST53>
2017-12-28 17:18:44 +0200

    Revert "add hw_5 ex.13"
    This reverts commit ceae08f4c8eca1a7d3e1b14055ec56d24cc78db9.

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -167,14 +167,5 @@ int main()
 		}
 	}
 	cout << "gorinich prokovtnuv " << (s_vas + s_gor) << " shashok" << endl;*/
-
-	//ex.12
-	/*int N, h = 1, i;
-	cin >> N;
-	for (i = 1; i < N; ++i, N -= h) {
-		cout << "den: " << i << " vismiknuto: " << h << " kilkist volosin: " << (N - h) << endl;
-		h *= 2;
-	}
-	cout << "volossja zakinchitsja za " << i << " dniv" << endl;*/
 	return 0;
 }
