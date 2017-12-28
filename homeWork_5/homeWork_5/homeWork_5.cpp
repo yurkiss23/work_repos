f7567eee4781d3eb1c559380ed8d6b0ad3ef6140
Администратор <@TEST53>
2017-12-28 17:51:51 +0200

    add hw_5 ex.13

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
-	
+	//ex.13
+	double N, rest;
+	int M, i, j;
+	cout << "input leght and stopday: ";
+	cin >> N >> M;
+	rest = N;
+	for (i = 1, j = 2; i != M; ++i, ++j) {
+		N /= j;
+		rest -= N;
+		cout << "den: " << i << " z'jv: " << N << " zalishilos: " << rest << endl;
+	}
+	cout << "na den: " << i << " zalishilos z'jsti: " << rest << "km" << endl;
 	return 0;
 }
