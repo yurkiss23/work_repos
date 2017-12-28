d6322fe97f9e381d2357f3b05693f1e59356a7ac
Администратор <@TEST53>
2017-12-28 16:14:48 +0200

    add hw_5 ex.10

--- homeWork_5/homeWork_5/homeWork_5.cpp
+++ homeWork_5/homeWork_5/homeWork_5.cpp
@@ -130,5 +130,24 @@ int main()
 	else {
 		cout << "vvedeno 1" << endl;
 	}*/
+	
+	//ex.10
+	/*double x, total = 1, div;
+	int n;
+	cout << "input digit and grade: ";
+	cin >> x >> n;
+	if (n > 0) {
+		for (int i = 0; i < n; ++i) {
+			total *= x;
+		}
+		cout << "result: " << total << endl;
+	}
+	else {
+		for (int i = 0; i < -n; ++i) {
+			total *= x;
+			div = 1 / total;
+		}
+		cout << "result: " << div << endl;
+	}*/
 	return 0;
 }
